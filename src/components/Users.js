c1da9bd55d281cccdefb67a1639328746579300a
afsarequebal <afsarequebal@gmail.com>
2023-04-03 16:10:50 -0400

    crud commit

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,10 +1,12 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import useAxiosPrivate from "../hooks/useAxiosPrivate";
 import { useNavigate, useLocation } from "react-router-dom";
 import { themes } from "devextreme/ui/themes";
 import "devextreme/dist/css/dx.dark.css";
 import DataGrid, {
   Column,
+  Editing,
+  Lookup,
   Grouping,
   GroupPanel,
   Pager,
@@ -12,12 +14,146 @@ import DataGrid, {
   SearchPanel,
 } from "devextreme-react/data-grid";
 import axios from "../api/axios";
+
 const Users = () => {
   const [users, setUsers] = useState();
   const axiosPrivate = useAxiosPrivate();
   const navigate = useNavigate();
   const location = useLocation();
 
+  const [editingRowKey, setEditingRowKey] = useState("");
+  const [editedRowData, setEditedRowData] = useState({
+    id: "",
+    username: "",
+    password: "",
+    email: "",
+  });
+  const dataGridRef = useRef(null);
+
+  // const startEditing = (rowKey, rowData) => {
+  //   // console.log("fordien");
+
+  //   setEditingRowKey(rowKey);
+  //   // console.log(data.id);
+  //   console.log(editingRowKey);
+  //   setEditedRowData({ ...rowData });
+  // };
+
+  // const cancelEditing = () => {
+  //   setEditingRowKey(null);
+  //   setEditedRowData({ id: "", username: "", password: "", email: "" });
+  // };
+
+  const onSaving = async (data) => {
+    // get the Datagrid instance
+    // console.log(data);
+    // console.log(data["changes"][0].data.email);
+    //console.log(data["changes"][0].data["email"]);
+    let id = data["changes"][0].key;
+    console.log(users);
+    if (data["changes"][0]["type"] === "update") {
+      const filteredResults = users.filter(
+        (user) => user.id === data["changes"][0].key
+      );
+      let myObject = Object.keys(data["changes"][0].data);
+      myObject.forEach(function (key, index) {
+        filteredResults[0][key] = data["changes"][0].data[key];
+      });
+
+      try {
+        const response = await axios.put(
+          "/api/users/" + id,
+          filteredResults[0],
+          {
+            headers: {
+              "Content-Type": "application/json",
+              "Access-Control-Allow-Origin": "*",
+            },
+            withCredentials: true,
+          }
+        );
+        setUsers(
+          users.map((user) => (user.id === id ? { ...response.data } : user))
+        );
+        console.log(users);
+        //setUsers(users);
+      } catch (err) {
+        console.error(err);
+        // navigate("/login", { state: { from: location }, replace: true });
+      }
+    } else if (data["changes"][0]["type"] === "remove") {
+      try {
+        const response = await axios.delete("/api/users/" + id, {
+          headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+          },
+          withCredentials: true,
+        });
+        setUsers(
+          //users.map((user) => (user.id === id ? { ...response.data } : user))
+          users.filter((user) => user.id !== id)
+        );
+        console.log(users);
+        // setUsers(users);
+      } catch (err) {
+        console.error(err);
+        // navigate("/login", { state: { from: location }, replace: true });
+      }
+    } else if (data["changes"][0]["type"] === "insert") {
+      // const filteredResults = users.filter(
+      //   (user) => user.id === data["changes"][0].key
+      // );
+      var user = {};
+      let myObject = Object.keys(data["changes"][0].data);
+      myObject.forEach(function (key, index) {
+        user[key] = data["changes"][0].data[key];
+      });
+
+      try {
+        const response = await axios.post("/api/users/", user, {
+          headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+          },
+          withCredentials: true,
+        });
+        // const userList = users.filter(
+        //   (userData) => userData.username !== user.username
+        // );
+        setUsers(
+          users.map((userData) =>
+            userData.username === user["username"]
+              ? { ...response.data }
+              : userData
+          )
+        );
+        // setUsers(userList);
+        // setUsers([...userList, response.data]);
+        console.log(users);
+        //setUsers(users);
+      } catch (err) {
+        console.error(err);
+        // navigate("/login", { state: { from: location }, replace: true });
+      }
+    }
+
+    // console.log(filteredResults[0]);
+    //console.log(data.changes[0].type);
+    //console.log(data.changes[0].data);
+  };
+
+  // const saveEditing = () => {
+  //   // get the Datagrid instance
+  //   const dataGrid = dataGridRef.current.instance;
+
+  //   // update the edited row data in the Datagrid
+  //   dataGrid.updateRow(editingRowKey, editedRowData);
+
+  //   // reset the editing state
+  //   setEditingRowKey(null);
+  //   setEditedRowData({ id: "", username: "", password: "", email: "" });
+  // };
   useEffect(() => {
     let isMounted = true;
     const controller = new AbortController();
@@ -35,7 +171,7 @@ const Users = () => {
         //   signal: controller.signal,
         // });
         //console.log(response);
-        console.log(response.data);
+        // console.log(response.data);
         isMounted && setUsers(response.data);
       } catch (err) {
         console.error(err);
@@ -52,6 +188,7 @@ const Users = () => {
   }, []);
   const pageSizes = [10, 25, 50, 100];
   const columns = ["id", "username", "password", "email"];
+
   return (
     // <article>
     //   <h2>Users List</h2>
@@ -68,10 +205,77 @@ const Users = () => {
     <div>
       <DataGrid
         dataSource={users}
-        keyExpr="ID"
+        keyExpr="id"
+        //  ref={dataGridRef}
+        onSaving={onSaving}
+        // editing={{
+        //   mode: "row",
+        //   allowUpdating: true,
+        //   allowDeleting: true,
+        //   allowAdding: true,
+        //   onEditingStart: (e) => {
+        //     console.log("not forde");
+        //     if (editingRowKey !== e.data.id) {
+        //       e.cancel = true;
+        //     }
+        //   },
+        // }}
+      >
+        <Editing
+          mode="row"
+          allowUpdating={true}
+          allowDeleting={true}
+          allowAdding={true}
+        />
+        {/* <Column dataField="id" caption="ID" allowEditing={true} /> */}
+        <Column dataField="username" caption="Username" allowEditing={true} />
+        <Column dataField="password" caption="Password" allowEditing={true} />
+        <Column dataField="email" caption="Email" allowEditing={true} />
+        {/* <Column
+          caption="Actions"
+          cellRender={(cellData) => {
+            const data = cellData.data;
+            const rowKey = data.id;
+            const rowData = data;
+            // console.log(rowKey);
+            //  console.log("here");
+            // console.log(rowData);
+            if (editingRowKey === rowKey) {
+              return (
+                <>
+                  <button onClick={saveEditing}>Save</button>
+                  <button onClick={cancelEditing}>Cancel</button>
+                </>
+              );
+            } else {
+              return (
+                <button
+                  style={{ fontSize: "20px" }}
+                  onClick={() => startEditing(rowKey, rowData)}
+                >
+                  Edit
+                </button>
+              );
+            }
+          }}
+        /> */}
+      </DataGrid>
+
+      {/* <DataGrid
+        dataSource={users}
+        keyExpr="id"
         defaultColumns={columns}
         showBorders={true}
-      />
+      >
+        <Paging enabled={true} />
+
+        <Editing
+          mode="row"
+          allowUpdating={true}
+          allowDeleting={true}
+          allowAdding={true}
+        />
+      </DataGrid> */}
       {/* <DataGrid
         dataSource={users}
         allowColumnReordering={true}
